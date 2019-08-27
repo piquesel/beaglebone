0dd6841eb6a2fe28601c12bba6f2dcb39adea211
piquesel <piquesel@me.com>
2019-08-27 09:16:07 +0200

    Fixing paths, /dev/ and adding debug

--- deploy-beaglebone.sh
+++ deploy-beaglebone.sh
@@ -1,34 +1,40 @@
 #!/bin/sh
 
+set -x
+
 OVH_SERVER="91.121.146.6"
 OVH_HOME="/home/desktop"
 BBB_IMAGES="$OVH_HOME/bbb/build/tmp/deploy/images/beaglebone"
 MACHINE="beaglebone"
 BBB_TMP="$MACHINE-tmp"
 
-mkdir $BBB_TMP 
+rm -rf $BBB_TMP
+mkdir -p $BBB_TMP 
 
 # Downloading all files and images from OVH server
 # MLO
-sudo scp desktop@$OVH_SERVER:$OVH_HOME/$BBB_IMAGES/MLO-{$MACHINE} ./$BBB_TMP/MLO
+echo "scp desktop@$OVH_SERVER:$BBB_IMAGES/MLO-${MACHINE} ./$BBB_TMP/MLO"
+scp desktop@$OVH_SERVER:$BBB_IMAGES/MLO-${MACHINE} ./$BBB_TMP/MLO
 
 # u-boot
-sudo scp desktop@$OVH_SERVER:$OVH_HOME/$BBB_IMAGES/{$MACHINE}.img ./$BBB_TMP/u-boot.img/
+scp desktop@$OVH_SERVER:$BBB_IMAGES/u-boot-${MACHINE}.img ./$BBB_TMP/u-boot.img
 
 # zImage
-sudo scp desktop@$OVH_SERVER:$OVH_HOME/$BBB_IMAGES/zImage ./$BBB_TMP/
+scp desktop@$OVH_SERVER:$BBB_IMAGES/zImage ./$BBB_TMP/
 
 if [ "x${1}" = "x" ]; then
 	echo "Usage: ${0} <block device>"
 	exit 0
 fi
 
-if [ -b ${1} ]; then
+if [ -b "/dev/${1}1" ]; then
 	DEV=/dev/${1}1
 else
-	echo "Block device not found: /dev/${1}"
+	echo "Block device not found: /dev/${1}1"
 #	exit 1
 fi
+
+exit 1
 
 echo "Formatting FAT partition on $DEV"
 sudo mkfs.vfat ${DEV}
